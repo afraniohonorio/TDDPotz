89c4817d734de22c8e2779b7fe1edf26c71e3325
Afrânio Honório de Oliveira <afranio@outlook.com>
2014-06-02 00:01:18 -0300

    criando página web

---
+++ TP_TDD_Potz/Home.aspx.designer.cs
@@ -0,0 +1,26 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     O código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código é regenerado. 
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace TP_TDD_Potz
+{
+
+
+    public partial class Home
+    {
+
+        /// <summary>
+        /// Controle form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+    }
+}
